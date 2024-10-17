393a63506157b4704529aad829e3807815f7a89b
ben-kotvis <kotvisbj@gmail.com>
2024-10-17 11:21:44 +0000

    more stuff

--- api/Program.cs
+++ api/Program.cs
@@ -4,15 +4,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
-//app.MapPost("/initialize", async (
-//    CancellationToken cancellationToken) => 
-//{
-   CopyDirectory.Copy(Path.Combine(Directory.GetCurrentDirectory(), "config/blackbox"), "/blackbox-config", true);
-   CopyDirectory.Copy(Path.Combine(Directory.GetCurrentDirectory(), "config/prometheus"), "/prometheus-config", true);
-   CopyDirectory.Copy(Path.Combine(Directory.GetCurrentDirectory(), "data/prometheus"), "/prometheus-data", true);
-   CopyDirectory.Copy(Path.Combine(Directory.GetCurrentDirectory(), "config/grafana"), "/grafana-config", true);
-   CopyDirectory.Copy(Path.Combine(Directory.GetCurrentDirectory(), "data/grafana"), "/grafana-data", true);
+   CopyDirectory.Copy(Path.Combine(Directory.GetCurrentDirectory(), "config/blackbox"), "/app/blackbox-config", true);
+   CopyDirectory.Copy(Path.Combine(Directory.GetCurrentDirectory(), "config/prometheus"), "/app/prometheus-config", true);
+   CopyDirectory.Copy(Path.Combine(Directory.GetCurrentDirectory(), "data/prometheus"), "/app/prometheus-data", true);
+   CopyDirectory.Copy(Path.Combine(Directory.GetCurrentDirectory(), "config/grafana"), "/app/grafana-config", true);
+   CopyDirectory.Copy(Path.Combine(Directory.GetCurrentDirectory(), "data/grafana"), "/app/grafana-data", true);
      
-//});
 
 app.Run();
