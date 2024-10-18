7ecbf6e819dfad79d957bf871e53db2322b92be8
ben-kotvis <kotvisbj@gmail.com>
2024-10-18 01:27:27 +0000

    some final changes

--- api/Program.cs
+++ api/Program.cs
@@ -4,11 +4,21 @@ var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
+app.MapGet("/", async() =>
+{
+   string fileName = Path.Combine(Directory.GetCurrentDirectory(), "index.html");
+        string fileContents = await System.IO.File.ReadAllTextAsync(fileName);
+    return Results.Content(fileContents, "text/html");
+});
+
+app.MapPost("/initialize",  (
+    CancellationToken cancellationToken) => 
+{
    CopyDirectory.Copy(Path.Combine(Directory.GetCurrentDirectory(), "config/blackbox"), "/app/blackbox-config", true);
    CopyDirectory.Copy(Path.Combine(Directory.GetCurrentDirectory(), "config/prometheus"), "/app/prometheus-config", true);
    CopyDirectory.Copy(Path.Combine(Directory.GetCurrentDirectory(), "data/prometheus"), "/app/prometheus-data", true);
-   CopyDirectory.Copy(Path.Combine(Directory.GetCurrentDirectory(), "config/grafana"), "/app/grafana-config", true);
+   CopyDirectory.Copy(Path.Combine(Directory.GetCurrentDirectory(), "config/grafana/provisioning"), "/app/grafana-config", true);
    CopyDirectory.Copy(Path.Combine(Directory.GetCurrentDirectory(), "data/grafana"), "/app/grafana-data", true);
-     
+});
 
 app.Run();
